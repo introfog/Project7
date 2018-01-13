f8382c8a0374970a431ff4a24a498a0388724fdb
Chubrick Dima <chubrickdm@gmail.com>
2018-01-13 19:59:16 +0300

    New technology parse XML file was started!

--- core/src/com/introfog/addition/parsers/ParseBasis.java
+++ core/src/com/introfog/addition/parsers/ParseBasis.java
@@ -8,22 +8,45 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.*;
 
-import java.io.IOException;
-import java.net.URLDecoder;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.Charset;
 
 public abstract class ParseBasis{
 	protected static boolean isFromIDEA = true; //флаг, хранящий откуда мы запускаем проект, с IDEA или с .jar архива
 	
 	
+	private static void currentDir2 (){
+		URL location = ParseBasis.class.getProtectionDomain ().getCodeSource ().getLocation ();
+		
+		String classLocation = null;
+		
+		try{
+			classLocation = URLDecoder.decode (location.getFile ().substring (1).replace ('/', File.separatorChar),
+											   Charset.defaultCharset ().name ()) + ParseBasis.class.getName ().replace (
+					'.', File.separatorChar) + ".class";
+		}
+		catch (UnsupportedEncodingException e){
+			System.out.println ("Encoding ERROR!");
+		}
+		
+		System.out.println (classLocation);
+	}
+	
 	protected static Document getDocument (String fromIDEA, String fromDesktop){
 		Document document;
+		XMLStreamReader xmlr;
 		try{
 			DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance ().newDocumentBuilder ();
 			
+			currentDir2 ();
+			
 			URLDecoder decoder = new URLDecoder ();
 			//строкой ниже мы полчили абсолютный путь к месту где находится файл класса ParseBasis
-			StringBuilder path = new StringBuilder (decoder.decode (ParseBasis.class.getProtectionDomain ().getCodeSource ().getLocation ().getPath ()));
+			StringBuilder path = new StringBuilder (decoder.decode (
+					ParseBasis.class.getProtectionDomain ().getCodeSource ().getLocation ().getPath ()));
 			int index = path.lastIndexOf (GameSystem.NAME_JAR_ARCHIVE); //ищем в этом пути имя архива
 			if (index == -1){ //если не нашли, значит мы запускаем проект с IDEA
 				isFromIDEA = true;
@@ -35,10 +58,37 @@ public abstract class ParseBasis{
 				path.append (fromDesktop);
 				document = documentBuilder.parse (path.toString ());
 			}
-			return document;
 			
+			return document;
 		}
 		catch (SAXException | IOException | ParserConfigurationException ex){
+			ex.printStackTrace (System.out);
+			return null;
+		}
+	}
+	
+	protected static XMLStreamReader getXML (String fromIDEA, String fromDesktop){
+		XMLStreamReader xmlr;
+		try{
+			URLDecoder decoder = new URLDecoder ();
+			StringBuilder path = new StringBuilder (decoder.decode (
+					ParseBasis.class.getProtectionDomain ().getCodeSource ().getLocation ().getPath ()));
+			int index = path.lastIndexOf (GameSystem.NAME_JAR_ARCHIVE); //ищем в этом пути имя архива
+			if (index == -1){ //если не нашли, значит мы запускаем проект с IDEA
+				isFromIDEA = true;
+				xmlr = XMLInputFactory.newInstance ().createXMLStreamReader (fromIDEA, new FileInputStream (fromIDEA));
+			}
+			else{ //если нашли, значит запускаем с .jar архива
+				isFromIDEA = false;
+				path.delete (index, path.length ()); //удаляем все начиная с имя архива
+				path.append (fromDesktop);
+				xmlr = XMLInputFactory.newInstance ().createXMLStreamReader (path.toString (),
+																			 new FileInputStream (path.toString ()));
+			}
+			
+			return xmlr;
+		}
+		catch (IOException | XMLStreamException ex){
 			ex.printStackTrace (System.out);
 			return null;
 		}
--- core/src/com/introfog/addition/parsers/ParseLevel.java
+++ core/src/com/introfog/addition/parsers/ParseLevel.java
@@ -12,6 +12,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.xml.stream.*;
+
 import static com.introfog.mesh.objects.GameObject.ASPECT_RATIO;
 
 public abstract class ParseLevel extends ParseBasis{
@@ -29,6 +31,39 @@ public abstract class ParseLevel extends ParseBasis{
 		LevelManager.NUM_TILE_H = Integer.parseInt (map.getAttributes ().item (0).getTextContent ());
 		levelH *= LevelManager.NUM_TILE_H;
 		levelH *= ASPECT_RATIO;
+	}
+	
+	private static void additionalCalculates2 (XMLStreamReader xmlReader){
+		//int tileW;
+		int tileH = 0;
+		try{
+			if (xmlReader.hasNext ()){
+				xmlReader.next ();
+				if (xmlReader.isStartElement () && xmlReader.getLocalName ().compareTo ("map") == 0){
+					for (int i = 0; i < xmlReader.getAttributeCount (); i++){
+						switch (xmlReader.getAttributeName (i).toString ()){
+						case "width":
+							LevelManager.NUM_TILE_W = Integer.parseInt (xmlReader.getAttributeValue (i));
+							break;
+						case "height":
+							LevelManager.NUM_TILE_H = Integer.parseInt (xmlReader.getAttributeValue (i));
+							break;
+						/*case "tilewidth":
+							tileW = Integer.parseInt (xmlReader.getAttributeValue (i));
+							break;*/
+						case "tileheight":
+							tileH = Integer.parseInt (xmlReader.getAttributeValue (i));
+							break;
+						}
+					}
+				}
+			}
+		}
+		catch (XMLStreamException | NumberFormatException | NullPointerException ex){
+			ex.printStackTrace (System.out);
+		}
+		
+		levelH = (int) (LevelManager.NUM_TILE_H * tileH * ASPECT_RATIO);
 	}
 	
 	private static void parseCoordinates (Node object){
@@ -86,9 +121,9 @@ public abstract class ParseLevel extends ParseBasis{
 	}
 	
 	
-	public static void parseLVL (int level){
+	public static void parseLVL3 (int level){
 		Document document = getDocument ("core/assets/xml/levels/lvl" + String.valueOf (level) + ".tmx",
-				"/resource/xml/levels/lvl" + String.valueOf (level) + ".tmx");
+										 "/resource/xml/levels/lvl" + String.valueOf (level) + ".tmx");
 		Node map = document.getDocumentElement ();
 		additionalCalculates (map);
 		
@@ -97,7 +132,8 @@ public abstract class ParseLevel extends ParseBasis{
 			Node objectGroup = objectGroups.item (i);
 			
 			if (objectGroup.getNodeType () != Node.TEXT_NODE){
-				String currObjectGroup = objectGroup.getAttributes ().item (0).getTextContent (); //запоминаем имя группы
+				String currObjectGroup = objectGroup.getAttributes ().item (
+						0).getTextContent (); //запоминаем имя группы
 				
 				NodeList objects = objectGroup.getChildNodes ();
 				for (int j = 0; j < objects.getLength (); j++){
@@ -110,6 +146,51 @@ public abstract class ParseLevel extends ParseBasis{
 					}
 				}
 			}
+		}
+	}
+	
+	public static void parseLVL (int level){
+		XMLStreamReader xmlReader = getXML ("core/assets/xml/levels/lvl" + String.valueOf (level) + ".tmx",
+											"/resource/xml/levels/lvl" + String.valueOf (level) + ".tmx");
+		additionalCalculates2 (xmlReader);
+		
+		String currObjectGroup = "";
+		try{
+			while (xmlReader.hasNext ()){
+				xmlReader.next ();
+				if (xmlReader.isStartElement ()){
+					if (xmlReader.getAttributeName (0).toString ().compareTo ("name") == 0){
+						currObjectGroup = xmlReader.getAttributeValue (0);
+					}
+					else if (xmlReader.getLocalName ().compareTo ("object") == 0){
+						for (int i = 0; i < xmlReader.getAttributeCount (); i++){
+							switch (xmlReader.getAttributeName (i).toString ()){
+							case "x":
+								x = Float.parseFloat (xmlReader.getAttributeValue (i));
+								x *= ASPECT_RATIO;
+								break;
+							case "y":
+								y = Float.parseFloat (xmlReader.getAttributeValue (i));
+								
+								break;
+							case "width":
+								w = Float.parseFloat (xmlReader.getAttributeValue (i));
+								w *= ASPECT_RATIO;
+								break;
+							case "height":
+								h = Float.parseFloat (xmlReader.getAttributeValue (i));
+								h *= ASPECT_RATIO;
+								break;
+							}
+						}
+						y = levelH - y * ASPECT_RATIO - h;
+						createObject (currObjectGroup);
+					}
+				}
+			}
+		}
+		catch (XMLStreamException ex){
+			ex.printStackTrace (System.out);
 		}
 	}
 }
